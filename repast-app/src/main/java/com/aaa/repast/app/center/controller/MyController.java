273f16e6bf7a3676e648cde3db1f5c9e69c0e12b
teacherChen <86521760@qq.com>
2019-11-04 22:39:48 +0800

    提交人：chenjian 提交原因：初始化创建

---
+++ repast-app/src/main/java/com/aaa/repast/app/center/controller/MyController.java
@@ -0,0 +1,12 @@
+package com.aaa.repast.app.center.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: 陈建
+ * @Date: 2019/11/4 0004 22:39
+ * @Version 1.0
+ */
+@RestController
+public class MyController {
+}
